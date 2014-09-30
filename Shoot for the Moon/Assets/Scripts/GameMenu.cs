a16090fe6df434fe853497109d99b1bba1f85c98
Corey Keller <cakeller43@gmail.com>
2014-09-30 10:38:00 -0500

    Added menu

---
+++ Shoot for the Moon/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour 
+{
+	public bool isQuit;
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.Escape)) 
+		{
+						Application.Quit ();
+		}
+	}
+
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		if (isQuit)
+						Application.Quit ();
+				else
+						Application.LoadLevel (1);
+	}
+}
